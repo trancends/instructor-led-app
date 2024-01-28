6b0a5bece4e3f3154e920d41f2aa214b7523c3c9
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-01-28 20:36:09 +0700

    [TESTING] ADD test for all method except for update,delete and insertcsv

--- repository/user_repository_test.go
+++ repository/user_repository_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"enigmaCamp.com/instructor_led/model"
+	sharedmodel "enigmaCamp.com/instructor_led/shared/shared_model"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/suite"
 )
@@ -19,22 +20,36 @@ type UserRepositoryTestSuite struct {
 	repo    UserRepository
 }
 
-var currentTime = time.Now().Local()
+var (
+	currentTime  = time.Now().Local()
+	expectedUser = model.User{
+		ID:       "1",
+		Name:     "test",
+		Email:    "test@example.com",
+		Password: "test",
+		Role:     "PARTICIPANT",
+	}
 
-var expectedUser = model.User{
-	ID:       "1",
-	Name:     "test",
-	Email:    "test@example.com",
-	Password: "test",
-	Role:     "PARTICIPANT",
-}
+	expectedUserError = model.User{
+		ID:       "1",
+		Name:     "test",
+		Password: "test",
+		Role:     "PARTICIPANT",
+	}
 
-var expectedUserError = model.User{
-	ID:       "1",
-	Name:     "test",
-	Password: "test",
-	Role:     "PARTICIPANT",
-}
+	expectedUsers = []model.User{
+		{ID: "1", Name: "User1", Email: "user1@example.com", Role: "PARTICIPANT"},
+		{ID: "2", Name: "User2", Email: "user2@example.com", Role: "PARTICIPANT"},
+		// Add more expected users as needed
+	}
+
+	expectedPaging = sharedmodel.Paging{
+		Page:        1,
+		RowsPerPage: 10,
+		TotalRows:   len(expectedUsers),
+		TotalPages:  1,
+	}
+)
 
 func (s *UserRepositoryTestSuite) SetupTest() {
 	db, mock, _ := sqlmock.New()
@@ -65,6 +80,118 @@ func (s *UserRepositoryTestSuite) TestCreate_Failed() {
 	err := s.repo.Create(expectedUserError)
 	s.NotNil(err)
 	s.Error(err)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByEmail_Success() {
+	rows := sqlmock.NewRows([]string{"id", "name", "email", "password", "role"}).AddRow(expectedUser.ID, expectedUser.Name, expectedUser.Email, expectedUser.Password, expectedUser.Role)
+	s.mockSql.
+		ExpectQuery(regexp.QuoteMeta("SELECT id, name, email, password, role FROM users WHERE email = $1 AND deleted_at IS NULL")).
+		WithArgs(expectedUser.Email).
+		WillReturnRows(rows)
+
+	user, err := s.repo.GetUserByEmail(expectedUser.Email)
+	s.Nil(err)
+	s.Equal(expectedUser, user)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByEmail_Failed() {
+	s.mockSql.
+		ExpectQuery(regexp.QuoteMeta("SELECT id, name, email, password, role FROM users WHERE email = $1 AND deleted_at IS NULL")).
+		WithArgs(expectedUser.Email).
+		WillReturnError(errors.New("sql: no rows in result set"))
+	_, err := s.repo.GetUserByEmail(expectedUser.Email)
+	s.NotNil(err)
+	s.Error(err)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByID_Success() {
+	rows := sqlmock.NewRows([]string{"id", "name", "email", "role"}).AddRow(expectedUser.ID, expectedUser.Name, expectedUser.Email, expectedUser.Role)
+	s.mockSql.
+		ExpectQuery(regexp.QuoteMeta("SELECT id, name, email, role FROM users WHERE id = $1 AND deleted_at IS NULL")).
+		WithArgs(expectedUser.ID).
+		WillReturnRows(rows)
+
+	user, err := s.repo.GetUserByID(expectedUser.ID)
+	s.Nil(err)
+	s.Equal(expectedUser.Email, user.Email)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByID_Failed() {
+	s.mockSql.
+		ExpectQuery(regexp.QuoteMeta("SELECT id, name, email, role FROM users WHERE id = $1 AND deleted_at IS NULL")).
+		WithArgs(expectedUser.ID).
+		WillReturnError(errors.New("sql: no rows in result set"))
+	_, err := s.repo.GetUserByID(expectedUser.ID)
+	s.NotNil(err)
+	s.Error(err)
+}
+
+func (s *UserRepositoryTestSuite) TestListSuccess() {
+	rows := sqlmock.NewRows([]string{"id", "name", "email", "role"})
+	for _, user := range expectedUsers {
+		rows.AddRow(user.ID, user.Name, user.Email, user.Role)
+	}
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, email, role FROM users WHERE deleted_at IS NULL ORDER BY created_at DESC LIMIT $1 OFFSET $2`)).
+		WithArgs(expectedPaging.RowsPerPage, (expectedPaging.Page-1)*expectedPaging.RowsPerPage).
+		WillReturnRows(rows)
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM users")).
+		WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(len(expectedUsers)))
+
+	users, paging, err := s.repo.List(expectedPaging.Page, expectedPaging.RowsPerPage)
+
+	s.NoError(err)
+	s.Equal(expectedUsers, users)
+	s.Equal(expectedPaging, paging)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByRole() {
+	rows := sqlmock.NewRows([]string{"id", "name", "email", "role"})
+	for _, user := range expectedUsers {
+		rows.AddRow(user.ID, user.Name, user.Email, user.Role)
+	}
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, email, role FROM users WHERE role = $3 AND deleted_at IS NULL ORDER BY created_at DESC LIMIT $1 OFFSET $2`)).
+		WithArgs("PARTICIPANT", expectedPaging.RowsPerPage, (expectedPaging.Page-1)*expectedPaging.RowsPerPage).
+		WillReturnRows(rows)
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM users WHERE role = $1")).
+		WithArgs("PARTICIPANT").
+		WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(len(expectedUsers)))
+
+	users, paging, err := s.repo.GetUserByRole(expectedUser.Role, expectedPaging.Page, expectedPaging.RowsPerPage)
+
+	s.NoError(err)
+	s.Equal(expectedUsers, users)
+	s.Equal(expectedPaging, paging)
+}
+
+func (s *UserRepositoryTestSuite) TestGetUserByRoleNoRows() {
+	s.mockSql.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, email, role FROM users WHERE role = $3 AND deleted_at IS NULL ORDER BY created_at DESC LIMIT $1 OFFSET $2`)).
+		WithArgs(expectedUser.Role, expectedPaging.RowsPerPage, (expectedPaging.Page-1)*expectedPaging.RowsPerPage).
+		WillReturnError(errors.New("sql: no rows in result set"))
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM users WHERE role = $1")).
+		WithArgs(expectedUser.Role).
+		WillReturnError(errors.New("sql: no rows in result set"))
+
+	_, _, err := s.repo.GetUserByRole(expectedUser.Role, expectedPaging.Page, expectedPaging.RowsPerPage)
+	s.Error(err)
+	s.NotNil(err)
+}
+
+func (s *UserRepositoryTestSuite) TestListFailed() {
+	s.mockSql.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, email, role FROM users WHERE deleted_at IS NULL ORDER BY created_at DESC LIMIT $1 OFFSET $2`)).
+		WithArgs(expectedPaging.RowsPerPage, (expectedPaging.Page-1)*expectedPaging.RowsPerPage).
+		WillReturnError(errors.New("sql: no rows in result set"))
+
+	s.mockSql.ExpectQuery(regexp.QuoteMeta("SELECT COUNT(*) FROM users")).
+		WillReturnError(errors.New("sql: no rows in result set"))
+
+	_, _, err := s.repo.List(expectedPaging.Page, expectedPaging.RowsPerPage)
+	s.Error(err)
+	s.NotNil(err)
 }
 
 func TestUserRepositoryTestSuite(t *testing.T) {

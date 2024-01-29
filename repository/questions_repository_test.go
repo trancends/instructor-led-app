7918b587b8c48e311292d0670cb9e0874f33f633
mohamatadilsentosa <mohamatadilsentosa@gmail.com>
2024-01-29 14:37:38 +0800

    questions repo

--- repository/questions_repository_test.go
+++ repository/questions_repository_test.go
@@ -3,10 +3,7 @@ package repository
 import (
 	"database/sql"
 	"testing"
-	"time"
 
-	"enigmaCamp.com/instructor_led/model"
-	sharedmodel "enigmaCamp.com/instructor_led/shared/shared_model"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/suite"
 )
@@ -17,68 +14,6 @@ type QuestionsRepositoryTestSuite struct {
 	mockSql sqlmock.Sqlmock
 	repo    QuestionsRepository
 }
-
-var (
-	currentTime      = time.Now()
-	expectedQuestion = model.Question{
-		ID:          "1",
-		UserID:      "1",
-		ScheduleID:  "1",
-		Description: "test",
-		Status:      "PROCESS",
-		CreatedAt:   nil,
-		UpdatedAt:   nil,
-		DeletedAt:   nil,
-	}
-	expectedQuestionsError = model.Question{
-		ID:          "1",
-		ScheduleID:  "1",
-		Description: "test",
-		Status:      "PROCESS",
-		CreatedAt:   nil,
-		UpdatedAt:   nil,
-		DeletedAt:   nil,
-	}
-	expectedQuestionsUpdate = model.Question{
-		ID:          "1",
-		UserID:      "1",
-		ScheduleID:  "1",
-		Description: "test",
-		Status:      "FINISHED",
-		CreatedAt:   &currentTime,
-		UpdatedAt:   &currentTime,
-		DeletedAt:   nil,
-	}
-	expectedQuestions = []model.Question{
-		{
-			ID:          "1",
-			UserID:      "1",
-			ScheduleID:  "1",
-			Description: "test",
-			Status:      "PROCESS",
-			CreatedAt:   nil,
-			UpdatedAt:   nil,
-			DeletedAt:   nil,
-		},
-		{
-			ID:          "2",
-			UserID:      "2",
-			ScheduleID:  "1",
-			Description: "test",
-			Status:      "FINISHED",
-			CreatedAt:   nil,
-			UpdatedAt:   nil,
-			DeletedAt:   nil,
-		},
-	}
-
-	expectedPaging = sharedmodel.Paging{
-		Page:        1,
-		RowsPerPage: 10,
-		TotalRows:   len(expectedQuestions),
-		TotalPages:  0,
-	}
-)
 
 func (s *QuestionsRepositoryTestSuite) SetupTest() {
 	db, mock, _ := sqlmock.New()
